09c7cef728f7ba0ddaafe23da61e849a2a84c3dd
SK SARDHAR MEIA <sksardar371@gmail.com>
2026-03-17 22:53:24 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -15,7 +15,7 @@ db.connect((err) => {
   if (err) {
     console.log('Database connection failed:', err);
   } else {
-    console.log('✅ Connected to MySQL database.');
+    console.log('Connected to MySQL database.');
   }
 });
 
